25797385a039479f7182f8046b48fee5ab2adc91
TeHe4K <hryblackwii@gmail.com>
2025-10-13 18:35:19 +0500

    Разработанный класс Sport. Коневский

---
+++ Classes/Sport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_8_Konevskii.Classes
+{
+    internal class Sport:Shop
+    {
+        public string Size {  get; set; }
+        public Sport(string Name, int Price, string Size) : base(Name,Price) 
+        {
+            this.Size = Size;
+        }
+    }
+}
