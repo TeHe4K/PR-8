16c7d3f5ef55af973aa4e8668c0bcd3accaf7c09
Kageru <hryblackwii@gmail.com>
2026-05-02 01:54:54 +0500

    Разработка общего класса. Коневский

---
+++ Classes/Common/DBCConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_8_Konevskii.Classes.Common
+{
+    public class DBCConnection
+    {
+        public static readonly string Path = @"";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source" + Path);
+            oleDbConnection.Open();
+            return oleDbConnection;
+        }
+        public static OleDbDataReader Query(string Query, OleDbConnection Connection)
+        {
+            return new OleDbCommand(Query, Connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(OleDbConnection Connection)
+        {
+            Connection.Close();
+        }
+    }
+}
