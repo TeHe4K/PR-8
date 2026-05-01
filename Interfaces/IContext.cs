d1de96afe67c5999d0c5da07bf45786153841be3
Kageru <hryblackwii@gmail.com>
2026-05-02 01:48:54 +0500

    Разработанные интерфейсы. Коневский

---
+++ Interfaces/IContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_8_Konevskii.Interfaces
+{
+    public interface IContext
+    {
+        List<object> All();
+        void Save(bool Update = false);
+
+        void Delete();
+    }
+}
